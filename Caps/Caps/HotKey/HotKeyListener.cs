2cedc4d030527b6299b06456ad84e828857f7be5
MolyMade.Yoga <MolyMade.Yoga@DESKTOP-SAN6UUS>
2016-10-24 22:22:28 +0800

    Update

--- Caps/Caps/HotKey/HotKeyListener.cs
+++ Caps/Caps/HotKey/HotKeyListener.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caps.HotKey.Structures;
+using Caps.KeyBoard;
+using Caps.KeyBoard.Structures;
 
 namespace Caps.HotKey
 {
-	class HotKeyListener
+	public class HotKeyListener
 	{
+		private KeyboardHook _keyboardHook;
+		public event EventHandler<HotKeyEventArgs> HotKeyTriggered;
+
+		public HotKeyListener()
+		{
+			this._keyboardHook = new KeyboardHook(KeyboardEventCallback);
+		}
+
+		private  bool KeyboardEventCallback(int vkCode, KeyboardMessages keyboardMessages, uint time)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnHotKeyTriggered()
+		{
+			HotKeyTriggered?.Invoke(this,new HotKeyEventArgs(HotKeyGroup.a));
+		}
 	}
 }
---
+++ Caps/Caps/HotKey/Structures/HotKeyEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caps.HotKey.Structures
+{
+	public class HotKeyEventArgs:EventArgs
+	{
+		public HotKeyGroup HotKey { get; internal set; }
+
+		public HotKeyEventArgs(HotKeyGroup hotKey)
+		{
+			this.HotKey = hotKey;
+		}
+	}
+}
