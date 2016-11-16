6cd22b1b26f71069f229036b011384f35b865dc7
MolyMade.Yoga <MolyMade.Yoga@DESKTOP-SAN6UUS>
2016-11-16 22:36:11 +0800

    Update

--- Caps/Caps/CapShow/QueryBoxItem.xaml.cs
+++ Caps/Caps/CapShow/QueryBoxItem.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -25,6 +26,30 @@ namespace Caps.CapShow
 		public QueryBoxItem()
 		{
 			InitializeComponent();
+		}
+
+		[Description("Get or set query text.")]
+		[Category("Common Properties")]
+		public string QueryText
+		{
+			get { return QueryTextBlock.Text; }
+			set { QueryTextBlock.Text = value; }
+		}
+
+		[Description("Get or set description text.")]
+		[Category("Common Properties")]
+		public string DescriptionText
+		{
+			get { return DescriptionTextBlock.Text; }
+			set { DescriptionTextBlock.Text = value; }
+		}
+
+		[Description("Get or set query icon.")]
+		[Category("Common Properties")]
+		public BitmapImage QueryIcon
+		{
+			get { return QueryIconBlock.Source as BitmapImage;}
+			set { QueryIconBlock.Source = value; }
 		}
 	}
 }
